e50d33827e53db9d5a34a6e32790252ee5689a97
Jérémy Sorant <jsorant@gmail.com>
2024-02-20 16:14:26 +0100

    Implement HeaderComponent

--- apps/personal-journey-front/src/app/app.component.cy.ts
+++ apps/personal-journey-front/src/app/app.component.cy.ts
@@ -1,12 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {
-  RouterLink,
-  RouterLinkActive,
-  RouterModule,
-  RouterOutlet,
-} from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe(AppComponent.name, () => {
--- apps/personal-journey-front/src/app/navigation/header/header.component.ts
+++ apps/personal-journey-front/src/app/navigation/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
+const HOME_ROUTE = '/home';
+const ADD_EVENT_ROUTE = '/events/add';
+const LIST_EVENTS_ROUTE = '/events/list';
 
 @Component({
   selector: 'duckrulz-header',
@@ -19,7 +23,17 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  public onToggleSidenav = () => {
-    console.log('TOGGLE');
-  };
+  router = inject(Router);
+
+  async onHomeClicked(): Promise<void> {
+    await this.router.navigateByUrl(HOME_ROUTE);
+  }
+
+  async onListClicked() {
+    await this.router.navigateByUrl(LIST_EVENTS_ROUTE);
+  }
+
+  async onAddClicked() {
+    await this.router.navigateByUrl(ADD_EVENT_ROUTE);
+  }
 }
