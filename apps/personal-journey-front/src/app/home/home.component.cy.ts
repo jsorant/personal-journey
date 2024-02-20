3a64169ad80a8839f1fcbe5f7c531e71d84273c5
Jérémy Sorant <jsorant@gmail.com>
2024-02-20 20:29:20 +0100

    Added HomeComponent

---
+++ apps/personal-journey-front/src/app/home/home.component.cy.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+describe('HeaderComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should have the home icon button', () => {
+    cy.get('#title').should('be.visible');
+    cy.get('#title').should('have.text', 'Welcome !');
+  });
+});
