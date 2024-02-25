2c49401499cbefefb99b2ff5fa4f404e5cd412bf
Jérémy Sorant <jsorant@gmail.com>
2024-02-25 08:55:36 +0100

    Added describe situation

--- apps/personal-journey-front/src/domain/situation.ts
+++ apps/personal-journey-front/src/domain/situation.ts
@@ -1,4 +1,3 @@
-import { CurrentDate } from '../shared-kernel/current-date';
 import { PhysicalSymptoms } from './physical-symptoms';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -6,34 +5,72 @@ declare namespace Situation {
   type SituationBuilder = typeof Situation.SituationBuilder.prototype;
 }
 
+export interface SituationDescription {
+  date: Date;
+  location: string;
+  description: string;
+}
+
 export class Situation {
-  readonly date: Date;
-  physicalSymptoms: PhysicalSymptoms[];
+  readonly creationDate: Date;
+  readonly physicalSymptoms: PhysicalSymptoms[];
+  readonly description?: SituationDescription;
 
   private constructor(builder: Situation.SituationBuilder) {
-    this.date = builder.date;
+    this.creationDate = builder.creationDate;
     this.physicalSymptoms = builder.physicalSymptoms;
+    this.description = builder.description;
   }
-  static builder(currentDate: CurrentDate): Situation.SituationBuilder {
-    return new Situation.SituationBuilder(currentDate);
+  static builder(): Situation.SituationBuilder {
+    return new Situation.SituationBuilder();
+  }
+
+  private static builderWithCurrentState(
+    situation: Situation
+  ): Situation.SituationBuilder {
+    return new Situation.SituationBuilder().withSituation(situation);
+  }
+
+  isDescribed(): boolean {
+    return this.description !== undefined;
+  }
+
+  describeSituation(description: SituationDescription) {
+    return Situation.builderWithCurrentState(this)
+      .withDescription(description)
+      .build();
   }
 
   static SituationBuilder = class {
-    date: Date;
+    creationDate: Date = new Date();
     physicalSymptoms: PhysicalSymptoms[] = [];
-
-    constructor(currentDate: CurrentDate) {
-      this.date = currentDate.value();
-    }
+    description?: SituationDescription;
 
     build(): Situation {
       return new Situation(this);
+    }
+
+    withCreationDate(date: Date): Situation.SituationBuilder {
+      this.creationDate = date;
+      return this;
+    }
+
+    withSituation(situation: Situation): Situation.SituationBuilder {
+      this.creationDate = situation.creationDate;
+      this.physicalSymptoms = situation.physicalSymptoms;
+      this.description = situation.description;
+      return this;
     }
 
     withPhysicalSymptoms(
       symptoms: PhysicalSymptoms[]
     ): Situation.SituationBuilder {
       this.physicalSymptoms = symptoms;
+      return this;
+    }
+
+    withDescription(description: SituationDescription) {
+      this.description = description;
       return this;
     }
   };
